1b021ff1f4fee16821e4794e34788767a065233b
Ambogo2 <ruthambogo.ra@gmail.com>
2024-01-23 08:41:26 -0800

    update

--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
 
